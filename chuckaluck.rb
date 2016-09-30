34c3a17a0e5ac1bd5f91eee0bfb3efa57728800b
William Patrick O Brien <obrienwp@dcsdk12.org>
2016-09-30 13:48:59 -0600

    Create chuckaluck.rb

--- chuckaluck.rb
+++ chuckaluck.rb
@@ -1,8 +1,3 @@
-#display options for betting, assign each a number
-#ask user for number
-#ask user for bet
-#have computer roll three Dice
-#create if statement for each bet
 
 puts '''
 Choose a bet type:
@@ -14,14 +9,27 @@ Choose a bet type:
 '''
 print "Enter number of bet type:"
 type = gets.chomp.to_i
+while type > 5 || type < 1
+  print "Enter a number 1 to 5: "
+  type = gets.chomp.to_i
+end
+
 roll = [rand(7), rand(7), rand(7)]
 
 def sum(arr)
   return arr[0] + arr[1] + arr[2]
 end
 
-print "How much do you bet?"
+money = 100
+
+
+print "How much do you bet? You have $#{money}."
 amt = gets.chomp.to_i
+while amt > money
+  print "You don't have that much money! Enter a smaller number: "
+  amt = gets.chomp.to_i
+end
+money -= amt
 
 if type == 1
   print "What number will appear?"
@@ -42,21 +50,21 @@ elsif type == 2
     win = false
   end
 elsif type == 3
-  puts "The roll is #{roll}.  The sum is #{sum(roll)}"
+  puts "The roll is #{roll}.  The sum is #{sum(roll)}."
   if sum(roll) >= 11
     win = true
   else
     win = false
   end
 elsif type == 4
-  puts "The roll is #{roll}. The sum is #{sum(roll)}"
+  puts "The roll is #{roll}. The sum is #{sum(roll)}."
   if sum(roll) <= 10
     win = true
   else
     win = false
   end
 elsif type == 5
-  puts "The roll is #{roll}. The sum is #{sum(roll)}"
+  puts "The roll is #{roll}. The sum is #{sum(roll)}."
   if sum(roll) <= 8 || sum(roll) >= 12
     win = true
   else
@@ -66,6 +74,18 @@ end
 
 if win
   puts "You win!"
+  if type == 2
+    money += (31 * amt)
+  else
+    money += (2 * amt)
+  end
 else
   puts "You lose!"
+end
+
+puts "You now have $#{money}."
+
+
+if money == 0
+  puts "You're broke!"
 end
